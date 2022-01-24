9ac6a7d927201a15b343d642f92006116383f8aa
Clayton Santos <claytondesousasantos@gmail.com>
2022-01-24 18:21:47 -0300

    melhorando a api

---
+++ forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,44 @@
+package br.com.alura.forum.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alura.forum.modelo.Curso;
+import junit.framework.Assert;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+	
+	@Autowired
+	private CursoRepository repository;
+	
+	@Autowired
+	private TestEntityManager em;
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+		String nomeCurso = "HTML 5";
+		
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programacao");
+		em.persist(html5);
+		
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNotNull(nomeCurso, curso.getNome());
+		Assert.assertEquals(nomeCurso, curso.getNome());
+	}
+
+}
