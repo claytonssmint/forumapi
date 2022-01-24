704096bff82a90aabe863b7ca50ad1bc1616fc5b
ClaytonSantosM8 <87668886+claytonssmint@users.noreply.github.com>
2022-01-24 19:16:30 -0300

    Delete Alura - JavaScript directory

--- Alura - JavaScript/meuPrimeiroPrograma.js
+++
@@ -1,9 +0,0 @@
-console.log("Meu Primeiro Programa: trabalhando com variáveis");
-
-const idade = 35;
-
-console.log(idade);
-console.log(idade+2);
-
-const idadeSomada = idade+2;
-console.log(idadeSomada);
